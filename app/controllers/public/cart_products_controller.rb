b262ebbc8dd7087b97bb9dfd9a70ebf2e7e0b821
EC2 Default User <ec2-user@ip-172-31-34-76.ec2.internal>
2021-07-19 02:55:02 +0000

    ショッピングカート機能ユーザー関連付け追加

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -6,13 +6,14 @@ class Public::CartProductsController < ApplicationController
 
   def create
     @product = Product.find(params[:id])
+    @cart_product.customer_id = current_customer_id
     @cart_product = current_cart_product.cart_products.build(product_id: params[:product_id])
     @cart_product.save
     redirect_to public_cart_product
   end
 
   def update
-
+    
   end
 
   def destroy
