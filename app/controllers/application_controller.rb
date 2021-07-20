7ad0b3d65196a3d69c971945f9783706d1cd73cb
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2021-07-20 09:21:01 +0000

    ルーティン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  #before_action :check_admin
+  #before_action :login_requied
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-end
 
-  before_action :check_admin
-  #before_action :login_requied
-  private
-
+  
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-
   def login_requied
     redirect_to login_path unless current_user
   end
-
-
-end
 
 end
