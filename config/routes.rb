6d7cab6c80c2351752fd66974a8fc241f9da9ee1
EC2 Default User <ec2-user@ip-172-31-12-176.ec2.internal>
2021-07-21 05:32:12 +0000

    [update]新規会員登録とログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admins, controllers: {
     sessions:      'admin/sessions',
     passwords:     'admin/passwords',
     registrations: 'admin/registrations'
   }
-  
+
   namespace :admin do
     resources :order_details, only:[:update]
     resources :orders, only:[:index,:show,:update]
@@ -13,15 +13,17 @@ Rails.application.routes.draw do
     resources :products, only:[:index,:new,:create,:show,:edit,:update]
     resources :customers, only:[:index,:show,:edit,:update]
     root :to => "homes#top"
-   
+
   end
 
-  scope module: :public do
-    devise_for :users, controllers: {
+  devise_for :users, controllers: {
     sessions:      'public/users/sessions',
     passwords:     'public/users/passwords',
     registrations: 'public/users/registrations'
   }
+
+  scope module: :public do
+
    resources :products, only:[:index,:show,]
    resources :shippings, only:[:index,:create,:edit,:update]
    resource :users, only:[:show,:edit,:update]
@@ -34,7 +36,7 @@ Rails.application.routes.draw do
    delete 'cart_products/destroy_all'
    root :to => "homes#top"
    get 'homes/about'
-   
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
