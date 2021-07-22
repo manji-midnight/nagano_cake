a8140a91cb5058234da285cabfbf12ffb7b25912
EC2 Default User <ec2-user@ip-172-31-40-133.ec2.internal>
2021-07-22 04:54:59 +0000

    db変更

--- db/migrate/20210715080908_create_order_details.rb
+++ db/migrate/20210715080908_create_order_details.rb
@@ -1,11 +1,11 @@
 class CreateOrderDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :order_details do |t|
-      t.integer :product_id
-      t.integer :order_id
-      t.integer :quantity
-      t.integer :taxed_price
-      t.integer :production_status
+      t.integer :product_id, null: false
+      t.integer :order_id, null: false
+      t.integer :quantity, null: false
+      t.integer :taxed_price, null: false
+      t.integer :production_status, null: false
       t.timestamps
     end
   end
--- db/migrate/20210715080940_create_orders.rb
+++ db/migrate/20210715080940_create_orders.rb
@@ -1,13 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.integer :customer_id
-      t.integer :shipping_fee
-      t.integer :total_price
-      t.integer :payment_method
-      t.string :name
-      t.string :postcode
-      t.string :address
+      t.integer :customer_id, null: false
+      t.integer :shipping_fee, null: false
+      t.integer :total_price, null: false
+      t.integer :payment_method, null: false
+      t.string :name, null: false
+      t.string :postcode, null: false
+      t.string :address, null: false
       t.integer :order_status
       t.timestamps
     end
