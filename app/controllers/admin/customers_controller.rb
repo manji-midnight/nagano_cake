fe13e0a532bd7c6d3417271197466a1136756a9e
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2021-07-23 05:32:05 +0000

    配送

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,26 @@
 class Admin::CustomersController < ApplicationController
   def index
+     @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customers = Customer.find(params[:id])
+    customers.update(customer_params)
+    redirect_to admin_customers_path
+  end
+  
+  private
+  
+  
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :password, :delete_user)
   end
 end
