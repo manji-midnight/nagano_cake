48a05f110c7ae126339390b94e7ead2973eecbe1
EC2 Default User <ec2-user@ip-172-31-40-133.ec2.internal>
2021-07-23 05:39:57 +0000

    product完成

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
     has_many :order_details
     belongs_to :genre
     attachment :image
-    
+
     def  add_tax_price
         (self.price * 1.10).round
     end
