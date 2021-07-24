ac3fe291f1ecbb7174ebc29c9b2fdc2744b158ae
EC2 Default User <ec2-user@ip-172-31-40-133.ec2.internal>
2021-07-24 05:24:41 +0000

    注文履歴途中まで

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all.page(params[:page]).per(10)
   end
 
   def show
@@ -7,4 +8,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
   end
+  
+  
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :shipping_fee, :total_price, :payment_method, :name, :postcode, :address, :order_status)
+  end
+    
 end
