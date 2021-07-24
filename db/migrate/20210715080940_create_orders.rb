89cf3c369dd1ec9727cba91507d35dcbb1b63c0d
EC2 Default User <ec2-user@ip-172-31-12-176.ec2.internal>
2021-07-24 09:58:52 +0000

    [start]注文履歴詳細画面作成

--- db/migrate/20210715080940_create_orders.rb
+++ db/migrate/20210715080940_create_orders.rb
@@ -4,11 +4,11 @@ class CreateOrders < ActiveRecord::Migration[5.0]
       t.integer :customer_id, null: false
       t.integer :shipping_fee, null: false
       t.integer :total_price, null: false
-      t.integer :payment_method, null: false
-      t.string :name, null: false
+      t.integer :payment_method, null: false, default: 0
+      t.string :address_name, null: false
       t.string :postcode, null: false
       t.string :address, null: false
-      t.integer :order_status, null: false
+      t.integer :order_status, null: false, default: 0
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -73,11 +73,11 @@ ActiveRecord::Schema.define(version: 2021_07_17_051509) do
     t.integer "customer_id", null: false
     t.integer "shipping_fee", null: false
     t.integer "total_price", null: false
-    t.integer "payment_method", null: false
-    t.string "name", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.string "address_name", null: false
     t.string "postcode", null: false
     t.string "address", null: false
-    t.integer "order_status", null: false
+    t.integer "order_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
