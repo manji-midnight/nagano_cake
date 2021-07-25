13464234a59c443cb3c3f8473a583203d1bb7d6d
EC2 Default User <ec2-user@ip-172-31-34-76.ec2.internal>
2021-07-25 11:59:40 +0000

    管理者注文履歴詳細画面の修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrdersController < ApplicationController
 
   def index
     if params[:check] == "0"
-    @orders = Order.all.page(params[:page]).per(10)
+      @orders = Order.all.page(params[:page]).per(10)
     elsif params[:check] && (params[:check] != "0")
       @orders = Order.where(customer_id: params[:check]).page(params[:page]).per(8)
       @notice = "注文履歴のみ表示しています。全ての会員の履歴はページ上部のリンクからご覧ください。"
@@ -21,7 +21,7 @@ class Admin::OrdersController < ApplicationController
     if @order.update(order_params)
       if @order.order_status == 1
         @order_products.each do |order|
-         order.update(production_status: 1)
+          order.update(production_status: 1)
         end
         flash[:notice] = "注文ステータスが「入金確認」となったため、制作ステージが「制作待ち」に自動更新されました"
       else
