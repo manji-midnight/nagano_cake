e33ac28e5d295b579c8a51ea02c2e9dee7d0b2b6
EC2 Default User <ec2-user@ip-172-31-40-133.ec2.internal>
2021-07-25 04:11:04 +0000

    注文確認画面のレイアウト修正しました

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -17,7 +17,6 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
-    @genre.save
     redirect_to admin_genres_path
   end
 
