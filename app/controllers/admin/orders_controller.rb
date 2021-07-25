8e91c514eed20fe01b0cc26f3ecdc27f8b7873dc
EC2 Default User <ec2-user@ip-172-31-43-15.us-east-2.compute.internal>
2021-07-25 04:13:30 +0000

    注文履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,38 @@
 class Admin::OrdersController < ApplicationController
   def index
+    if params[:check] == "0"
     @orders = Order.all.page(params[:page]).per(10)
-  end
+    elsif params[:check] && (params[:check] != "0")
+      @orders = Order.where(customer_id: params[:check]).page(params[:page]).per(8)
+      @notice = "注文履歴のみ表示しています。全ての会員の履歴はページ上部のリンクからご覧ください。"
+    else
+      @orders = Order.all.page(params[:page]).per(8)
+    end
 
   def show
-  end
+     @order = Order.find(params[:id])
+     @order_products = @order.order_products
+     
 
-  def update
+   def update
+    @order = Order.find(params[:id])
+    @order_products = @order.ordered_products
+    if @order.update(order_params)
+      if @order.order_status == 1
+        @order_products.each do |order|
+          order.update(production_status: 1)
+        end
+        flash[:notice] = "注文ステータスが「入金確認」となったため、制作ステージが「制作待ち」に自動更新されました"
+      else
+        flash[:notice] = "注文ステータスを変更しました"
+      end
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:notice] = "注文ステータスを変更できませんでした"
+      render :show
+    end
+
+   end
   end
   
   
@@ -17,4 +43,5 @@ class Admin::OrdersController < ApplicationController
     params.require(:order).permit(:customer_id, :shipping_fee, :total_price, :payment_method, :name, :postcode, :address, :order_status)
   end
     
+  end
 end
